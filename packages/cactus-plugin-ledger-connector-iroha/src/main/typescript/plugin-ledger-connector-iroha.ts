6feff8c156714c636b4c41e889842d9bad7776c4
hxlaf <xuhan@lafayette.edu>
2021-06-24 14:40:28 -0500

    update openapi.json

--- packages/cactus-plugin-ledger-connector-iroha/src/main/typescript/plugin-ledger-connector-iroha.ts
+++ packages/cactus-plugin-ledger-connector-iroha/src/main/typescript/plugin-ledger-connector-iroha.ts
@@ -39,31 +39,20 @@ import {
 } from "@hyperledger/cactus-common";
 
 import {
-  WatchBlocksV1,
-  ConsistencyStrategy,
-  DeployContractSolidityBytecodeV1Request,
-  DeployContractSolidityBytecodeV1Response,
-  EthContractInvocationType,
   InvokeContractV1Request,
   InvokeContractV1Response,
-  ReceiptType,
   RunTransactionRequest,
   RunTransactionResponse,
-  SignTransactionRequest,
-  SignTransactionResponse,
-  Web3SigningCredentialCactusKeychainRef,
-  Web3SigningCredentialPrivateKeyHex,
-  Web3SigningCredentialType,
 } from "./generated/openapi/typescript-axios/";
 
 import { RunTransactionEndpoint } from "./web-services/run-transaction-endpoint";
-import { isWeb3SigningCredentialNone } from "./model-type-guards";
+//import { isWeb3SigningCredentialNone } from "./model-type-guards";
 import { PrometheusExporter } from "./prometheus-exporter/prometheus-exporter";
 import {
   GetPrometheusExporterMetricsEndpointV1,
   IGetPrometheusExporterMetricsEndpointV1Options,
 } from "./web-services/get-prometheus-exporter-metrics-endpoint-v1";
-import { WatchBlocksV1Endpoint } from "./web-services/watch-blocks-v1-endpoint";
+//import { WatchBlocksV1Endpoint } from "./web-services/watch-blocks-v1-endpoint";
 
 export const E_KEYCHAIN_NOT_FOUND = "cactus.connector.iroha.keychain_not_found";
 
@@ -84,7 +73,7 @@ export class PluginLedgerConnectorIroha
     ICactusPlugin,
     IPluginWebService {
   private readonly instanceId: string;
-  //public prometheusExporter: PrometheusExporter;
+  public prometheusExporter: PrometheusExporter;
   private readonly log: Logger;
   private readonly web3: Web3;
   private readonly pluginRegistry: PluginRegistry;
@@ -118,26 +107,26 @@ export class PluginLedgerConnectorIroha
     //this.web3 = new Web3(web3WsProvider);
     this.instanceId = options.instanceId;
     this.pluginRegistry = options.pluginRegistry;
-    // this.prometheusExporter =
-    //   options.prometheusExporter ||
-    //   new PrometheusExporter({ pollingIntervalInMin: 1 });
-    // Checks.truthy(
-    //   this.prometheusExporter,
-    //   `${fnTag} options.prometheusExporter`,
-    // );
+    this.prometheusExporter =
+      options.prometheusExporter ||
+      new PrometheusExporter({ pollingIntervalInMin: 1 });
+    Checks.truthy(
+      this.prometheusExporter,
+      `${fnTag} options.prometheusExporter`,
+    );
 
-    //this.prometheusExporter.startMetricsCollection();
+    this.prometheusExporter.startMetricsCollection();
   }
 
-  // public getPrometheusExporter(): PrometheusExporter {
-  //   return this.prometheusExporter;
-  // }
+  public getPrometheusExporter(): PrometheusExporter {
+    return this.prometheusExporter;
+  }
 
-  // public async getPrometheusExporterMetrics(): Promise<string> {
-  //   const res: string = await this.prometheusExporter.getPrometheusMetrics();
-  //   this.log.debug(`getPrometheusExporterMetrics() response: %o`, res);
-  //   return res;
-  // }
+  public async getPrometheusExporterMetrics(): Promise<string> {
+    const res: string = await this.prometheusExporter.getPrometheusMetrics();
+    this.log.debug(`getPrometheusExporterMetrics() response: %o`, res);
+    return res;
+  }
 
   public getInstanceId(): string {
     return this.instanceId;
@@ -164,13 +153,13 @@ export class PluginLedgerConnectorIroha
     const webServices = await this.getOrCreateWebServices();
     await Promise.all(webServices.map((ws) => ws.registerExpress(app)));
 
-    wsApi.on("connection", (socket: SocketIoSocket) => {
-      this.log.debug(`New Socket connected. ID=${socket.id}`);
+    // wsApi.on("connection", (socket: SocketIoSocket) => {
+    //   this.log.debug(`New Socket connected. ID=${socket.id}`);
 
-      socket.on(WatchBlocksV1.Subscribe, () => {
-        new WatchBlocksV1Endpoint({ web3, socket, logLevel }).subscribe();
-      });
-    });
+    //   socket.on(WatchBlocksV1.Subscribe, () => {
+    //     new WatchBlocksV1Endpoint({ web3, socket, logLevel }).subscribe();
+    //   });
+    // });
     return webServices;
   }
 
@@ -180,13 +169,7 @@ export class PluginLedgerConnectorIroha
     }
 
     const endpoints: IWebServiceEndpoint[] = [];
-    // {
-    //   const endpoint = new DeployContractSolidityBytecodeEndpoint({
-    //     connector: this,
-    //     logLevel: this.options.logLevel,
-    //   });
-    //   endpoints.push(endpoint);
-    // }
+
     {
       const endpoint = new RunTransactionEndpoint({
         connector: this,
@@ -201,13 +184,7 @@ export class PluginLedgerConnectorIroha
     //   });
     //   endpoints.push(endpoint);
     // }
-    // {
-    //   const endpoint = new BesuSignTransactionEndpointV1({
-    //     connector: this,
-    //     logLevel: this.options.logLevel,
-    //   });
-    //   endpoints.push(endpoint);
-    // }
+
     {
       const opts: IGetPrometheusExporterMetricsEndpointV1Options = {
         connector: this,
@@ -242,7 +219,7 @@ export class PluginLedgerConnectorIroha
     const fnTag = `${this.className}#transact()`;
     //commandName:string
     //params:Array<any>  
-    switch (req.web3SigningCredential.type) {
+    switch (req.commandName) {
       // Web3SigningCredentialType.GETHKEYCHAINPASSWORD is removed as Hyperledger Besu doesn't support the PERSONAL api
       // for --rpc-http-api as per the discussion mentioned here
       // https://chat.hyperledger.org/channel/besu-contributors?msg=GqQXfW3k79ygRtx5Q
@@ -265,7 +242,7 @@ export class PluginLedgerConnectorIroha
       }
       default: {
         throw new Error(
-          `${fnTag} Unrecognized Web3SigningCredentialType: ` +
+          `${fnTag} Unrecognized Transaction type: ` +
             `${req.web3SigningCredential.type} Supported ones are: ` +
             `${Object.values(Web3SigningCredentialType).join(";")}`,
         );
@@ -273,153 +250,5 @@ export class PluginLedgerConnectorIroha
     }
   }
 
-  public async transactSigned(
-    req: RunTransactionRequest,
-  ): Promise<RunTransactionResponse> {
-    const fnTag = `${this.className}#transactSigned()`;
 
-    Checks.truthy(req.consistencyStrategy, `${fnTag}:req.consistencyStrategy`);
-    Checks.truthy(
-      req.transactionConfig.rawTransaction,
-      `${fnTag}:req.transactionConfig.rawTransaction`,
-    );
-    const rawTx = req.transactionConfig.rawTransaction as string;
-    this.log.debug("Starting web3.eth.sendSignedTransaction(rawTransaction) ");
-    const txPoolReceipt = await this.web3.eth.sendSignedTransaction(rawTx);
-    this.log.debug("Received preliminary receipt from Besu node.");
-
-    if (txPoolReceipt instanceof Error) {
-      this.log.debug(`${fnTag} sendSignedTransaction failed`, txPoolReceipt);
-      throw txPoolReceipt;
-    }
-    this.prometheusExporter.addCurrentTransaction();
-
-    if (
-      req.consistencyStrategy.receiptType === ReceiptType.NodeTxPoolAck &&
-      req.consistencyStrategy.blockConfirmations > 0
-    ) {
-      throw new Error(
-        `${fnTag} Conflicting parameters for consistency` +
-          ` strategy: Cannot wait for >0 block confirmations AND only wait ` +
-          ` for the tx pool ACK at the same time.`,
-      );
-    }
-
-    switch (req.consistencyStrategy.receiptType) {
-      case ReceiptType.NodeTxPoolAck:
-        return { transactionReceipt: txPoolReceipt };
-      case ReceiptType.LedgerBlockAck:
-        this.log.debug("Starting poll for ledger TX receipt ...");
-        const txHash = txPoolReceipt.transactionHash;
-        const { consistencyStrategy } = req;
-        const ledgerReceipt = await this.pollForTxReceipt(
-          txHash,
-          consistencyStrategy,
-        );
-        this.log.debug(
-          "Finished poll for ledger TX receipt: %o",
-          ledgerReceipt,
-        );
-        return { transactionReceipt: ledgerReceipt };
-      default:
-        throw new Error(
-          `${fnTag} Unrecognized ReceiptType: ${req.consistencyStrategy.receiptType}`,
-        );
-    }
-  }
-
-  public async transactPrivateKey(
-    req: RunTransactionRequest,
-  ): Promise<RunTransactionResponse> {
-    const fnTag = `${this.className}#transactPrivateKey()`;
-    const { transactionConfig, web3SigningCredential } = req;
-    const {
-      secret,
-    } = web3SigningCredential as Web3SigningCredentialPrivateKeyHex;
-
-    const signedTx = await this.web3.eth.accounts.signTransaction(
-      transactionConfig,
-      secret,
-    );
-
-    if (signedTx.rawTransaction) {
-      req.transactionConfig.rawTransaction = signedTx.rawTransaction;
-      return this.transactSigned(req);
-    } else {
-      throw new Error(
-        `${fnTag} Failed to sign eth transaction. ` +
-          `signedTransaction.rawTransaction is blank after .signTransaction().`,
-      );
-    }
-  }
-
-  public async transactCactusKeychainRef(
-    req: RunTransactionRequest,
-  ): Promise<RunTransactionResponse> {
-    const fnTag = `${this.className}#transactCactusKeychainRef()`;
-    const { transactionConfig, web3SigningCredential } = req;
-    const {
-      ethAccount,
-      keychainEntryKey,
-      keychainId,
-    } = web3SigningCredential as Web3SigningCredentialCactusKeychainRef;
-
-    // locate the keychain plugin that has access to the keychain backend
-    // denoted by the keychainID from the request.
-    const keychainPlugin = this.pluginRegistry.findOneByKeychainId(keychainId);
-
-    Checks.truthy(keychainPlugin, `${fnTag} keychain for ID:"${keychainId}"`);
-
-    // Now use the found keychain plugin to actually perform the lookup of
-    // the private key that we need to run the transaction.
-    const privateKeyHex = await keychainPlugin?.get<string>(keychainEntryKey);
-
-    return this.transactPrivateKey({
-      transactionConfig,
-      web3SigningCredential: {
-        ethAccount,
-        type: Web3SigningCredentialType.PrivateKeyHex,
-        secret: privateKeyHex,
-      },
-      consistencyStrategy: {
-        blockConfirmations: 0,
-        receiptType: ReceiptType.NodeTxPoolAck,
-        timeoutMs: 60000,
-      },
-    });
-  }
-
-  public async pollForTxReceipt(
-    txHash: string,
-    consistencyStrategy: ConsistencyStrategy,
-  ): Promise<TransactionReceipt> {
-    const fnTag = `${this.className}#pollForTxReceipt()`;
-    let txReceipt;
-    let timedOut = false;
-    let tries = 0;
-    let confirmationCount = 0;
-    const timeoutMs = consistencyStrategy.timeoutMs || Number.MAX_SAFE_INTEGER;
-    const startedAt = new Date();
-
-    do {
-      tries++;
-      timedOut = Date.now() >= startedAt.getTime() + timeoutMs;
-      if (timedOut) {
-        break;
-      }
-
-      txReceipt = await this.web3.eth.getTransactionReceipt(txHash);
-      if (!txReceipt) {
-        continue;
-      }
-
-      const latestBlockNo = await this.web3.eth.getBlockNumber();
-      confirmationCount = latestBlockNo - txReceipt.blockNumber;
-    } while (confirmationCount >= consistencyStrategy.blockConfirmations);
-
-    if (!txReceipt) {
-      throw new Error(`${fnTag} Timed out ${timeoutMs}ms, polls=${tries}`);
-    }
-    return txReceipt;
-  }
 }
